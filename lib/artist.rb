97ecc609360bf836ee7a081c2c0a47cc9a794b66
Joy Lin <vvlnote@hotmail.com>
2019-03-26 17:36:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,10 +10,14 @@ class Artist
     @@all
   end
   def add_song(song)
-    puts "#{song}"
     self.class.all << song
     self.songs << song
     song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    new_song = Song.new(name)
+    self.add_song(new_song)
   end
   
   def self.song_count
