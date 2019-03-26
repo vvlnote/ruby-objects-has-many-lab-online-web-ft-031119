ea9d3cb3f05c2aa0f117f725b0756d8dec9da8d9
Joy Lin <vvlnote@hotmail.com>
2019-03-26 17:24:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,18 @@
 class Artist
   attr_accessor :name, :songs
-  
+  @@all = []
   def initialize(name)
     @name = name
     @songs = []
   end
   
+  def self.all
+    @@all
+  end
   def add_song(song)
+    self.class.all << song
     @songs << song
   end
+  
+  
 end
