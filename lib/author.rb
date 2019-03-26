b011a51a74a9360ab99f52135173b87fa3303387
Joy Lin <vvlnote@hotmail.com>
2019-03-26 17:44:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,27 @@
 class Author
   
   attr_accessor :name, :posts
-  
+  @@all = []
   def initialize(name)
     @name = name
     @posts = []
   end
   
+  def self.all
+    @@all
+  end
+  def add_post(post)
+    post.author = self
+    posts << post
+    self.class.all << post
+  end
   
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    add_post(new_post)
+  end
+  
+  def self.post_count
+    all.length
+  end
 end
