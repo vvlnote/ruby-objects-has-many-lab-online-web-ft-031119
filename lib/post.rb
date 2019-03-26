6d7c35e8315a719e32e3e2d3978c02a9ce60d844
Joy Lin <vvlnote@hotmail.com>
2019-03-26 17:40:51 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,21 @@
 class Post
   
+  attr_accessor :title, :author
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if author 
+      return author.name 
+    else
+      return nil 
+    end
+  end
 end
